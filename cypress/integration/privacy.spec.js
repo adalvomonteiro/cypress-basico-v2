22b2d06d592e26eeb4b2c14df42f9ed16eb4cd49
adalvo_monteiro <adalvo.monteiro@gmail.com>
2024-01-24 16:53:53 -0300

    Revert "- Comitt curso"
    This reverts commit eea38639780847746324efd5e488d6941653f7a6.

--- cypress/integration/privacy.spec.js
+++
@@ -1,7 +0,0 @@
-it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
-    cy.visit('./src/privacy.html')
-
-    cy.contains("Talking About Testing").should("be.visible")
-
-}
-)
--- cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit',function(){
-    const longtest = "teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste"
-        cy.get('input[name="firstName"]').type('Almir')
-        cy.get('#lastName').type('Sater')
-        cy.get('#email').type('teste@teste.com.br')
-        cy.get('#open-text-area').type(longtest,{delay:0})
-        cy.contains('button','Enviar').click()
-})
